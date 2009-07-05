962da7277586367dfd01db62b98f335cc6afac3f
Henrique Dias <heukirne@gmail.com>
2009-07-05 15:09:34 -0300

    Shared memory with mmap and mutex for concurrence

--- Solver.cpp
+++ Solver.cpp
@@ -2,27 +2,50 @@
 //Based in http://www2-fs.informatik.uni-tuebingen.de/~reinhard/krypto/English/4.5.1.e.html
 //Created by Heukirne
 
-#include <map>
 #include <sstream>
 #include <math.h>
 #include <stdlib.h>
+//POSIX Includes
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <iostream>
+#include <string>
 
 #include "BasicSSP.h"
 
 #define FACTOR 100
 
+struct list {
+    float key;
+    char value[20];
+};
+
+struct shared { 
+  sem_t mutex; /* the mutex: a Posix memory-based semaphore */ 
+  int count; /* and the counter */ 
+  list map[1000];
+} sharedStruct; 
+
 class SubsetSolver {
     int sum;
     vector<int> set;
     vector<int> table;
-    map<float,string> *solution;
+    shared *solution;
     int ini_set;
     int piece;
     float id; //constant split sides
     
     public:
     
-    SubsetSolver (vector<int> set_init, int sum_init, map<float,string> *solution_init, int piece_init) {
+    SubsetSolver (vector<int> set_init, int sum_init, shared *solution_init, int piece_init) {
         set = set_init;
         sum = sum_init;
         solution = solution_init;
@@ -30,6 +53,18 @@ class SubsetSolver {
     }
     
     private: 
+   
+    //inser value in shared memory with mutex control concurrence
+    void solution_insert(float key, string value) {
+        int length;
+        
+        sem_wait(&solution->mutex); 
+        printf("child:%d>", solution->count++); 
+        solution->map[solution->count].key = key;
+        length = value.copy(solution->map[solution->count].value,value.length(),0);
+        solution->map[solution->count].value[length] = '\0';
+        sem_post(&solution->mutex);   
+    }
     
     float calcSum() { //Add all selected numbers
         float result = id;
@@ -41,20 +76,22 @@ class SubsetSolver {
             result += set[ini_set+i] * table[i];
             hash += table[i]+48;
         }
-        solution->insert(make_pair(result, hash));
-        //cout << hash << "=" << result << ", ";
+        solution_insert(result, hash);
+        cout << hash << "=" << result << ", ";
         return result;
     }    
    
-    string find_value(float value) {
-        map<float,string>::iterator pos;
+    string find_value(float value, bool owner_id) {
         float frac; float i;
-        for(pos = solution->begin(); pos!=solution->end(); ++pos) {
-            if ((int)(value - pos->first) == 0) { //verifica se é o valor esperado
-                frac = FACTOR*modff(pos->first, &i);
-                if ((int)(frac - FACTOR*id) != 0) { //verifica se não é do próprio id
+        bool compare;
+        for(int pos = 0; pos<=solution->count; ++pos) {
+            if ((int)(value - solution->map[pos].key) == 0) { //verifica se é o valor esperado
+                frac = FACTOR*modff(solution->map[pos].key, &i);
+                compare = ((int)(frac - FACTOR*id) != 0);
+                if (owner_id == true) compare = !compare;
+                if (compare) { //verifica se não é do próprio id
                     //cout << endl << "compare: " << (int)(frac - FACTOR*id); 
-                    return pos->second;    
+                    return solution->map[pos].value;    
                 }
             }
         }
@@ -65,13 +102,14 @@ class SubsetSolver {
         table.at(ini)=b;
         if (ini+1>=table.capacity()) { 
             float result = calcSum() - id;
-            string other_piece = find_value(sum - result);
+            string other_piece = find_value(sum - result,false);
             if (other_piece.length() > 0) {
                 //sleep(1);
-                cout << endl << "SOLUTION(" << other_piece << "[" << (sum - result) << "] + [" << (result + id) << "]" << (*solution)[result + id] << ")";
+                string my_piece = find_value(sum - result,true);
+                cout << endl << "SOLUTION(" << other_piece << "[" << (sum - result) << "] + [" << (result + id) << "]" << my_piece << ")";
             }
         } else { //call recursion
-            //cout << "ini=" << ini << ", end=" << end << ", b=" << b << endl;
+            //cout << "ini=" << ini << ", b=" << b << endl;
             table_do(ini+1,0);
             table_do(ini+1,1);
         }
@@ -109,10 +147,18 @@ class SubsetSolver {
     
 };
 
+bool file_exists(string filename) {
+    struct stat stfileinfo;
+    if (stat(filename.c_str(),&stfileinfo)==0) {
+        return true;
+    }
+    return false;
+}
+
 struct thread_data {
     string values;
     int piece;
-    map<float,string> *solution;
+    shared *solution;
 };
 
 void* init_solver(void* params) {
@@ -139,21 +185,36 @@ void* init_solver(void* params) {
 pthread_t first_solver;
 pthread_t second_solver;
 pthread_attr_t attr;
-map<float,string> *solution;
 
 int main() {
     cout << "GET TO SUBSOLVER !\n";
+
+    //create mmap shared memory file
+    struct shared *ptr; 
+    string filename = "./file.o";
+    bool fileExist=false;
+    int fd;
+    
+    /* open file, initialize to 0, map into memory */ 
+    fileExist = file_exists(filename);
+    fd = open(filename.c_str(), O_RDWR | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO); 
+    if (!fileExist) { write(fd,&sharedStruct, sizeof(struct shared));  }
+    ptr = (struct shared *)mmap(NULL, sizeof(struct shared), PROT_READ | PROT_WRITE, MAP_SHARED,fd, 0); 
+    close(fd); 
+    
+    /* initialize semaphore that is shared between processes */ 
+    sem_init(&ptr->mutex,1, 1); 
+    setbuf(stdout,NULL);
     
     //Set struct data
-    map<float,string> solution;
     struct thread_data data[2];
     data[0].values = "1,3,6,7,12,2,4,5,8,9,10;70";
     data[0].piece = 1;
-    data[0].solution = &solution;
+    data[0].solution = ptr;
     
     data[1].values = "1,3,6,7,12,2,4,5,8,9,10;70";
     data[1].piece = 2;
-    data[1].solution = &solution;
+    data[1].solution = ptr;
     
     //Init Joinable Attribute
     pthread_attr_init(&attr);
@@ -161,7 +222,7 @@ int main() {
     
     //Create Threads Solvers
     pthread_create(&first_solver, &attr, &init_solver, &data[0]);
-    sleep(1);
+    //sleep(1);
     pthread_create(&second_solver, &attr, &init_solver, &data[1]);
 
     pthread_attr_destroy(&attr);
